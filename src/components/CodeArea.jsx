9f3599982c617d1cc5dea2718068dfd9c26d5e8b
SaloniSadhwani <salonisadhwani@gmail.com>
2023-08-26 19:40:33 +0530

    CodeMirror React

--- src/components/CodeArea.jsx
+++ src/components/CodeArea.jsx
@@ -1,18 +1,20 @@
-import React from "react";
 import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-
+import 'codemirror/addon/display/autorefresh';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
 
 export default function CodeArea() {
-    const onChange = React.useCallback((value, viewUpdate) => {
-        console.log('value:', value);
-      }, []);
-      return (
-        <CodeMirror
-          value="console.log('hello world!');"
-          height="200px"
-          extensions={[javascript({ jsx: true })]}
-          onChange={onChange}
-        />
-      );
+  const code = 'const a = 0;';
+
+  <CodeMirror
+    value={code}
+    options={{
+      theme: 'monokai',
+      tabSize: 2,
+      keyMap: 'sublime',
+      mode: 'jsx',
+    }}
+  />
 }
