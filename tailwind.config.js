bb3f3b98166bb5d03b1891ea5199312c1bc20d08
SaloniSadhwani <salonisadhwani@gmail.com>
2023-09-19 20:01:11 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         // Simple 8 row grid
         '10': 'repeat(10, minmax(0, 1fr))',
         '20': 'repeat(20, minmax(0, 1fr))',
+        '19': 'repeat(19, minmax(0, 1fr))',
       },
       gridRow: {
         'span-20' : 'span 20 / span 20',
