876c270814c63e9a9d2801e26fccee1fc8011525
SaloniSadhwani <salonisadhwani@gmail.com>
2023-10-17 21:27:51 +0530

    Made a few changes to the search box

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
         className="col-span-16 col-start-5 row-span-2 row-start-1 mb-0.5 ml-2 mr-4 rounded-lg border border-solid border-gray-400 bg-inherit"
       >
         <div id="searchbar" className='flex items-center justify-items-start '>
-          <input className='ml-2.5 mt-1.5 w-60 h-10 pl-2' placeholder='Search'></input>
+          <input className='ml-2.5 mt-1.5 w-60 h-10 pl-2 bg-inherit border border-solid border-[#94a3b8] focus:outline-none' placeholder='Search'></input>
           <button className='ml-2.5 mt-1 pl-4 w-14 h-10 border border-solid border-[#313467] bg-[#313467] text-white text-xl font-bold'> <BsSearch /> </button>
         </div>
         
